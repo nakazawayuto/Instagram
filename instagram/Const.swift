330aaa8e0f0631fd22be127c2d65b7dbfda3c303
中澤ゆうと <nakazawayuuto@nakazawayuutos-macbook-ea.local>
2018-12-06 16:10:29 +0900

    カリキュラム　終了

---
+++ instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  instagram
+//
+//  Created by 中澤ゆうと on 2018/12/03.
+//  Copyright © 2018年 nakazawayuto. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let PostPath = "posts"
+}
